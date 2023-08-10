efb21ba6cdb0f9b686b53290af71ccba5b75dd64
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-08-10 12:37:07 +0200

    creando un componente

---
+++ PracticandoConSvelte/src/UnPrimerComponente.svelte
@@ -0,0 +1,25 @@
+<script>
+    let nombre = "Mundo";
+</script>
+
+<main>
+    <h1>Hola {nombre} desde Svelte!</h1>
+    <p>¡Bienvenido a tu primera aplicación Svelte!</p>
+</main>
+
+<style>
+    main {
+        text-align: center;
+        padding: 1em;
+    }
+
+    h1 {
+        color: #ff6600;
+        font-size: 2.5em;
+    }
+
+    p {
+        color: #333;
+        font-size: 1.2em;
+    }
+</style>
